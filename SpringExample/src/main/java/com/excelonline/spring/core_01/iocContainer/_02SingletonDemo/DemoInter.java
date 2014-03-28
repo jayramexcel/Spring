cd4298d0e94be12e9f6997f89c21fc82934b8e48
Jayram <jayram.excel@gmail.com>
2014-03-27 22:28:42 -0400

    Update

--- SpringExample/src/main/java/com/excelonline/spring/core_01/iocContainer/_02SingletonDemo/DemoInter.java
+++ SpringExample/src/main/java/com/excelonline/spring/core_01/iocContainer/_02SingletonDemo/DemoInter.java
@@ -1,7 +1,5 @@
 package com.excelonline.spring.core_01.iocContainer._02SingletonDemo;
 
-
-public interface  DemoInter
-{
-      String wish(String uname);
+public interface DemoInter {
+	String wish(String uname);
 }
