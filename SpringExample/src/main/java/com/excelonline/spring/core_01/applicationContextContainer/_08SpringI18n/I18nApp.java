09af18e302b289ae4d826f2893fdb7a5c5a0420e
Jayram <jayram.rout@7chapters.info>
2014-03-30 10:01:26 -0400

    Update

--- SpringExample/src/main/java/com/excelonline/spring/core_01/applicationContextContainer/_08SpringI18n/I18nApp.java
+++ SpringExample/src/main/java/com/excelonline/spring/core_01/applicationContextContainer/_08SpringI18n/I18nApp.java
@@ -1,23 +1,28 @@
 package com.excelonline.spring.core_01.applicationContextContainer._08SpringI18n;
+
 import java.util.Locale;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.GetPackageInfo;
 
-
-
 public class I18nApp {
-	public static void main(String args[])throws Exception {
+	public static void main(String args[]) throws Exception {
 		System.out.println(GetPackageInfo.getInfo(I18nApp.class));
-		Locale locale = new Locale("en");
-		ClassPathXmlApplicationContext  ctx = new ClassPathXmlApplicationContext("com/excelonline/spring/core_01/applicationContextContainer/_08SpringI18n/applicationContext.xml");
+		Locale locale = new Locale("fr","be");
+//		Locale locale = new Locale("en","uk");
+//		Locale locale = new Locale("en","ca");
+//		Locale locale = new Locale("en");
 		
-		String msg = ctx.getMessage("str1", new String[]{"hi", "Jayram"}, "default msg", locale);
-		msg +=" "+ ctx.getMessage("str2", null, "default msg", locale);
-		msg +=" "+ ctx.getMessage("str3", null, "default msg", locale);
-		msg +=" "+ ctx.getMessage("str4", null, "default msg", locale);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"com/excelonline/spring/core_01/applicationContextContainer/_08SpringI18n/applicationContext.xml");
 
-		System.out.println("Message is: "+msg);
+		String msg = ctx.getMessage("str1", new String[] { "hi", "Jayram" },
+				"default msg", locale);
+		msg += " " + ctx.getMessage("str2", null, "default msg", locale);
+		msg += " " + ctx.getMessage("str3", null, "default msg", locale);
+		msg += " " + ctx.getMessage("str4", null, "default msg", locale);
+
+		System.out.println("Message is: " + msg);
 	}
 }
