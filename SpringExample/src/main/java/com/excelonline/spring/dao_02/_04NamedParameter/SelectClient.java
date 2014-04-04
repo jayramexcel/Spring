952432f96f29a01bc825a51bad526255c826ff3b
Jayram <jayram.excel@gmail.com>
2014-04-03 22:22:52 -0400

    Update

--- SpringExample/src/main/java/com/excelonline/spring/dao_02/_04NamedParameter/SelectClient.java
+++ SpringExample/src/main/java/com/excelonline/spring/dao_02/_04NamedParameter/SelectClient.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class   SelectClient {
         public static void main(String args[]) {
-			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/excel/dao_02/_04NamedParameter/applicationContext.xml");
+			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/excelonline/spring/dao_02/_04NamedParameter/applicationContext.xml");
 			SelectImple s=(SelectImple)ctx.getBean("sel");
 			System.out.println("Employees in between empid 100 to 130 count is: "+s.getEmpCount(100, 130));
         }
