efe51e7d6188074db588c6a52f3fdd89d1ad6533
Jayram <jayram.rout@7chapters.info>
2014-04-03 22:24:07 -0400

    Update

--- SpringExample/src/main/java/com/excelonline/spring/dao_02/_01ConnectionPooling/ThirdParty/_2ApacheDBCP/SelectInter.java
+++
@@ -1,7 +0,0 @@
-package com.excelonline.spring.dao_02._01ConnectionPooling.ThirdParty._2ApacheDBCP;
-
-public interface  SelectInter
-{
-	public String fetchEmpName(int eno);
-	public long fetchEmpSalary(int eno);
-}
